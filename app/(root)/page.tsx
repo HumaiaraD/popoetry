7d55493dd120a71a1e2707f299f1d7b221758495
Humaiara Diea <d.humaiara@gmail.com>
2025-12-05 00:58:45 -0500

    fixing auth issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import { auth } from "@/auth";
 import { sanityFetch} from "@/sanity/lib/live";
 import SanityLiveComponent from "@/components/SanityLiveComponent";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page({
   searchParams,
